ca5392a3c37dbfb81153dce614dd73e4601a5bf1
sarahelhawary <sarah.elhwarii92@gmail.com>
2021-03-29 11:57:20 +0200

    initiate reacr routes

---
+++ src/book-shelves.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class BookShelves extends Component {
+  render() {
+    return <div>BookShelves</div>
+  }
+}
+
+export default BookShelves
---
+++ src/search-page.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class SearchPage extends Component {
+  render() {
+    return <div>search page</div>
+  }
+}
+
+export default SearchPage
